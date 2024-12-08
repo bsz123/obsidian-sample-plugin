915d9e147041acd1a2af8969dd9b50a6c4d43368
Ben Zuke <ben.zuke@atomicobject.com>
2024-12-07 21:48:06 -0500

    Search semi working

--- main.ts
+++ main.ts
@@ -1,139 +1,44 @@
-// import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// // Remember to rename these classes and interfaces!
-
-// interface MyPluginSettings {
-// 	mySetting: string;
-// }
-
-// const DEFAULT_SETTINGS: MyPluginSettings = {
-// 	mySetting: 'default'
-// }
-
-// export default class MyPlugin extends Plugin {
-// 	settings: MyPluginSettings;
-
-// 	async onload() {
-// 		await this.loadSettings();
-
-// 		// This creates an icon in the left ribbon.
-// 		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-// 			// Called when the user clicks the icon.
-// 			new Notice('This is a notice!');
-// 		});
-// 		// Perform additional things with the ribbon
-// 		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-// 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-// 		const statusBarItemEl = this.addStatusBarItem();
-// 		statusBarItemEl.setText('Status Bar Text');
-
-// 		// This adds a simple command that can be triggered anywhere
-// 		this.addCommand({
-// 			id: 'open-sample-modal-simple',
-// 			name: 'Open sample modal (simple)',
-// 			callback: () => {
-// 				new SampleModal(this.app).open();
-// 			}
-// 		});
-// 		// This adds an editor command that can perform some operation on the current editor instance
-// 		this.addCommand({
-// 			id: 'sample-editor-command',
-// 			name: 'Sample editor command',
-// 			editorCallback: (editor: Editor, view: MarkdownView) => {
-// 				console.log(editor.getSelection());
-// 				editor.replaceSelection('Sample Editor Command');
-// 			}
-// 		});
-// 		// This adds a complex command that can check whether the current state of the app allows execution of the command
-// 		this.addCommand({
-// 			id: 'open-sample-modal-complex',
-// 			name: 'Open sample modal (complex)',
-// 			checkCallback: (checking: boolean) => {
-// 				// Conditions to check
-// 				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-// 				if (markdownView) {
-// 					// If checking is true, we're simply "checking" if the command can be run.
-// 					// If checking is false, then we want to actually perform the operation.
-// 					if (!checking) {
-// 						new SampleModal(this.app).open();
-// 					}
-
-// 					// This command will only show up in Command Palette when the check function returns true
-// 					return true;
-// 				}
-// 			}
-// 		});
-
-// 		// This adds a settings tab so the user can configure various aspects of the plugin
-// 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-// 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-// 		// Using this function will automatically remove the event listener when this plugin is disabled.
-// 		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-// 			console.log('click', evt);
-// 		});
-
-// 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-// 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-// 	}
-
-// 	onunload() {
-
-// 	}
-
-// 	async loadSettings() {
-// 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-// 	}
-
-// 	async saveSettings() {
-// 		await this.saveData(this.settings);
-// 	}
-// }
-
-// class SampleModal extends Modal {
-// 	constructor(app: App) {
-// 		super(app);
-// 	}
-
-// 	onOpen() {
-// 		const {contentEl} = this;
-// 		contentEl.setText('Woah!');
-// 	}
-
-// 	onClose() {
-// 		const {contentEl} = this;
-// 		contentEl.empty();
-// 	}
-// }
-
-// class SampleSettingTab extends PluginSettingTab {
-// 	plugin: MyPlugin;
-
-// 	constructor(app: App, plugin: MyPlugin) {
-// 		super(app, plugin);
-// 		this.plugin = plugin;
-// 	}
-
-// 	display(): void {
-// 		const {containerEl} = this;
-
-// 		containerEl.empty();
-
-// 		new Setting(containerEl)
-// 			.setName('Setting #1')
-// 			.setDesc('It\'s a secret')
-// 			.addText(text => text
-// 				.setPlaceholder('Enter your secret')
-// 				.setValue(this.plugin.settings.mySetting)
-// 				.onChange(async (value) => {
-// 					this.plugin.settings.mySetting = value;
-// 					await this.plugin.saveSettings();
-// 				}));
-// 	}
-// }
-
 import { App, Notice, Plugin, PluginSettingTab, Setting } from "obsidian";
+
+type CollectionHit = {
+	document: {
+		altTitle: string;
+		embedding: number[];
+		id: string;
+		imageUrl: string;
+		publishDateDay: number;
+		publishDateMonth: number;
+		publishDateTimestamp: number;
+		publishDateYear: number;
+		title: string;
+		topics: string[];
+		transcript: string;
+	};
+};
+// {
+// 		"document": {
+// 				"altTitle": "This only makes it more urgent that we adopt my roadmap for the next 10 years, which should put us solidly in the lead.",
+// 				"embedding": [0.232, 0.123, 0.345, 0.456, 0.567, 0.678, 0.789, 0.890, 0.901, 0.012],
+// 				"id": "2968",
+// 				"imageUrl": "https://imgs.xkcd.com/comics/university_age.png",
+// 				"publishDateDay": 5,
+// 				"publishDateMonth": 8,
+// 				"publishDateTimestamp": 1722816000,
+// 				"publishDateYear": 2024,
+// 				"title": "University Age",
+// 				"topics": [
+// 						"Incomplete explanations",
+// 						"Cueball",
+// 						"Public speaking",
+// 						"Time"
+// 				],
+// 				"transcript": "When I took the helm five years ago, our university was 213 years old – the second oldest in the state, just behind our 215 year old rival. Under my leadership, we've funded an intensive program to increase our age to 218, overtaking our rival by 3. Unfortunately, I have terrible news. "
+// 		},
+// 		"highlight": {},
+// 		"highlights": []
+// },
+
+type CollectionHits = CollectionHit[];
 
 interface XkcdFetcherSettings {
 	apiUrl: string;
@@ -145,12 +50,16 @@ const DEFAULT_SETTINGS: XkcdFetcherSettings = {
 
 export default class XkcdFetcherPlugin extends Plugin {
 	settings: XkcdFetcherSettings;
+	collectionHits: CollectionHits;
 
 	async onload() {
 		console.log("Loading XkcdFetcherPlugin");
 
 		// Load settings
 		await this.loadSettings();
+
+		// set up the collection
+		await this.setCollection();
 
 		// Add a command to fetch the first xkcd comic
 		this.addCommand({
@@ -180,12 +89,19 @@ export default class XkcdFetcherPlugin extends Plugin {
 	}
 
 	async fetchFirstComic() {
+		// this.parseCollectionForHit(0);
+		this.searchCollectionByTitle("Matter");
+	}
+
+	async setCollection() {
+		this.collectionHits = await this.fetchTypesense();
+		console.log("🔴🔵 this.collection", this.collectionHits);
+	}
+
+	async fetchTypesense() {
 		try {
 			// Construct the query URL
-			const query = ""; // Add your query here if needed
-			const url = `${
-				this.settings.apiUrl
-			}?xkcd%5Bquery%5D=${encodeURIComponent(query)}`;
+			const url = `https://qtg5aekc2iosjh93p.a1.typesense.net/collections/xkcd/documents/search?q=*&use_cache=true&x-typesense-api-key=8hLCPSQTYcBuK29zY5q6Xhin7ONxHy99`;
 
 			// Make the HTTP GET request
 			const response = await fetch(url);
@@ -196,20 +112,31 @@ export default class XkcdFetcherPlugin extends Plugin {
 			// Parse the JSON response
 			const data = await response.json();
 
-			if (Array.isArray(data) && data.length > 0) {
-				const firstComic = data[0];
-				// Display the comic information in a notice or handle as needed
-				new Notice(
-					`First xkcd Comic: #${firstComic.num} - ${firstComic.title}`
+			if (!data.hits) {
+				throw new Error(
+					"No hits found in the xkcd typesense collection."
 				);
-				console.log(firstComic);
-			} else {
-				new Notice("No comics found.");
 			}
+
+			return data.hits;
 		} catch (error) {
-			console.error("Error fetching xkcd comic:", error);
-			new Notice("Failed to fetch xkcd comic.");
+			console.error("Error fetching xkcd typsense collection:", error);
+			new Notice("Failed to fetch typsense collection.");
 		}
+	}
+	parseCollectionForHit(index: number) {
+		console.log("🔴🔵 collection", this.collectionHits);
+		console.log("🔴🔵 index", index);
+		console.log("🍪🔴🟡 collection[index]", this.collectionHits[index]);
+	}
+	searchCollectionByTitle(title: string) {
+		console.log("🔴🔵 title", title);
+
+		const hit = this.collectionHits.find((hit) =>
+			hit.document.title.includes(title)
+		);
+
+		console.log("🔴🔵 hit", hit);
 	}
 }
 
